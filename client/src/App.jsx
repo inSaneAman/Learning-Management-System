bc90b104b8bf1f6efcc00cabe329fc19a1940e3c
inSaneAman <162028654+inSaneAman@users.noreply.github.com>
2025-01-07 22:39:59 +0530

    ceated 404 not found page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,12 +4,14 @@ import { Route, Routes } from "react-router-dom";
 
 import AboutUs from "./pages/aboutUs.jsx";
 import HomePage from "./pages/homePage.jsx";
+import NotFound from "./pages/notFound.jsx";
 function App() {
     return (
         <>
             <Routes>
                 <Route path="/" element={<HomePage />}></Route>
                 <Route path="/about" element={<AboutUs />}></Route>
+                <Route path="*" element={<NotFound />}></Route>
             </Routes>
         </>
     );
