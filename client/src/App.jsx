5ab6273e60b3e6450b8d00fe8f717d0fdd021ee7
inSaneAman <162028654+inSaneAman@users.noreply.github.com>
2025-01-10 18:50:39 +0530

    Built access denied page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,6 +5,7 @@ import { Route, Routes } from "react-router-dom";
 import AboutUs from "./pages/aboutUs.jsx";
 import ContactUs from "./pages/contactUs.jsx";
 import CourseList from "./pages/Course/courseList.jsx";
+import Denied from "./pages/denied.jsx";
 import HomePage from "./pages/homePage.jsx";
 import Login from "./pages/login.jsx";
 import NotFound from "./pages/notFound.jsx";
@@ -20,6 +21,8 @@ function App() {
                 <Route path="/login" element={<Login />}></Route>
                 <Route path="/courses" element={<CourseList />}></Route>
                 <Route path="/contact" element={<ContactUs />}></Route>
+                <Route path="/denied" element={<Denied />}></Route>
+
                 <Route path="*" element={<NotFound />}></Route>
             </Routes>
         </>
