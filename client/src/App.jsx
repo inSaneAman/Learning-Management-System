73a8e22f40583b0ca495602c26ff70b6f540741b
inSaneAman <162028654+inSaneAman@users.noreply.github.com>
2025-01-10 19:42:10 +0530

    Added require auth page for authentication for role-specific tasks

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,16 +2,17 @@ import "./App.css";
 
 import { Route, Routes } from "react-router-dom";
 
+import RequireAuth from "./components/auth/requireAuth.jsx";
 import AboutUs from "./pages/aboutUs.jsx";
 import ContactUs from "./pages/contactUs.jsx";
 import CourseDescription from "./pages/Course/courseDescription.jsx";
 import CourseList from "./pages/Course/courseList.jsx";
+import CreateCourse from "./pages/Course/createCourse.jsx";
 import Denied from "./pages/denied.jsx";
 import HomePage from "./pages/homePage.jsx";
 import Login from "./pages/login.jsx";
 import NotFound from "./pages/notFound.jsx";
 import SignUp from "./pages/signUp.jsx";
-
 function App() {
     return (
         <>
@@ -27,6 +28,13 @@ function App() {
                     path="/course/description"
                     element={<CourseDescription />}
                 ></Route>
+
+                <Route element={<RequireAuth allowedRoles={["ADMIN"]} />}>
+                    <Route
+                        path="/course/create"
+                        element={<CreateCourse />}
+                    ></Route>
+                </Route>
 
                 <Route path="*" element={<NotFound />}></Route>
             </Routes>
